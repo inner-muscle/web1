1a6b4a6bbacaad5cee6f9f8539ff037ccec61c26
inner-muscle <tsuruua1223@gmail.com>
2017-07-23 00:41:29 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,1 +1,21 @@
+function getFileName()
+{
+  return window.location.href.split('/').pop();
+}
 
+var filename = getFileName();
+var opt;
+if(filename === 'sid.html')
+{
+  opt = document.querySelector('option[value="sid.html"]');
+}
+else
+{
+  opt = document.querySelector('option[value="index.html"]');
+}
+opt.selected = true;
+
+document.getElementById('form').select.onchange = function()
+{
+  location.href = document.getElementById('form').select.value;
+}
