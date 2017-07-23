4c16373aa13c6fda19b5f48b86c6696178db3646
inner-muscle <tsuruua1223@gmail.com>
2017-07-23 18:44:01 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -10,42 +10,42 @@ if(filename === 'sid.html')
   opt = document.querySelector('option[value="sid.html"]');
 }
 
-if(filename === 'joey.html')
+ else if(filename === 'joey.html')
 {
   opt = document.querySelector('option[value="joey.html"]');
 }
 
-if(filename === 'paul.html')
+else if(filename === 'paul.html')
 {
   opt = document.querySelector('option[value="paul.html"]');
 }
 
-if(filename === 'chris.html')
+else if(filename === 'chris.html')
 {
   opt = document.querySelector('option[value="chris.html"]');
 }
 
-if(filename === 'jim.html')
+else if(filename === 'jim.html')
 {
   opt = document.querySelector('option[value="jim.html"]');
 }
 
-if(filename === 'craig.html')
+else if(filename === 'craig.html')
 {
   opt = document.querySelector('option[value="craig.html"]');
 }
 
-if(filename === 'shawn.html')
+else if(filename === 'shawn.html')
 {
   opt = document.querySelector('option[value="shawn.html"]');
 }
 
-if(filename === 'mick.html')
+else if(filename === 'mick.html')
 {
   opt = document.querySelector('option[value="mickhtml"]');
 }
 
-if(filename === 'corey.html')
+else if(filename === 'corey.html')
 {
   opt = document.querySelector('option[value="corey.html"]');
 }
